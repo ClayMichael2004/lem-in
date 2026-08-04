6be0041a250d798ed5c3bf13e4d3a81e18da38aa
michaelochieng0 <mclay@kabarak.ac.ke>
2026-08-04 11:33:56 +0300

    adding the farm and links functions with the structures for the path and movements

--- pkg/antfarm/farm.go
+++ pkg/antfarm/farm.go
@@ -13,4 +13,27 @@ type Farm struct{
 	End *Room
 	Rooms map[string]*Room 
 	Links map[string][]string
+	RawInput  string
+}
+
+//new farm initializes a new farm struct
+func NewFarm() *Farm{
+	return &Farm{
+		Rooms: make(map[string] *Room),
+		Links: make(map[string][]string),
+	}
+}
+
+//Addlink adds an undirected link between 2 rooms
+func (f *Farm) AddLink(u, v string){
+	f.Links[u] = append(f.Links[u], v)
+	f.Links[v] = append(f.Links[v], u)
+}
+
+//path represents a route from ##start to ##end
+type Path []string
+
+type Movement struct{
+	AntID int
+	RoomName string
 }
